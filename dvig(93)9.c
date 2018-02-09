a3607901a8b15b371eb55cd190de22e013120618
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-09 13:54:53 +0530

    dvig(93)9.c

---
+++ dvig(93)9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("even number between 1 to 100\n");
+for(counter=1;counter<+100;counter++)
+{
+if(counter52==0)
+{
+printf("%d",counter);
+}
+}
+return 0;
+}
