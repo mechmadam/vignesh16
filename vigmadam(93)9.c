3865b9d6e3e7a70ab78401a79770f0b4edccef59
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-09 14:19:08 +0530

    vigmadam(93)9.c

---
+++ vigmadam(93)9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,result;
+clrscr();
+printf("Enter the number");
+scanf("%d",&n);
+if(n!=o)
+{
+rem=n%10;
+result=result+rem*rem*rem;
+n1=10;
+printf("Armstrong number");
+}
+else
+{
+printf("not an armstrong number");
+}
+getch();
+return 0;
+}
+
