a3d348f25b50ea463a0d5a43b4c02e703b6e919e
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-12 15:48:39 +0530

    vigneshmadam12.c

---
+++ vigneshmadam12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int fact,i,n;
+clrscr();
+printf("Enter the value of n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+fact=fact*i;
+printf("The Fact %d Fact);
+getch();
+}
