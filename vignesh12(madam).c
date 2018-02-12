dd2f6f09671499f567f258c65b926e3d23dbea0b
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-12 16:01:54 +0530

    vignesh(12)madam.c

---
+++ vignesh12(madam).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+longint sum=0,i,n;
+clrscr();
+printf("\n please give the value of n: ");
+scan("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n 5 * %ld =%ld"i,5*i);
+}
+getch();
+}
